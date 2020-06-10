b6cbc0ee506c6587a7cff50c4dac9a2d4933d502
SeyiAriyo <ariyoseyiariyo@gmail.com>
2020-06-10 17:28:58 -0400

    step 7

--- src/api.js
+++ src/api.js
@@ -5,7 +5,9 @@ const getItems = function () {
 };
 
 const createItem = function (name) {
-    const newItem = JSON.stringify({ name });
+    console.log(name)
+    const newItem = JSON.stringify({name});
+    
 
     return fetch(`${BASE_URL}/items`, {
         method: 'POST',
@@ -16,7 +18,22 @@ const createItem = function (name) {
     });
 };
 
+function updateItem(id, updateData) {
+    console.log(updateData);
+    const newData = JSON.stringify({updateData});
+
+    return fetch(`${BASE_URL}/items/${id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: newData
+    });
+}
+
 export default {
     getItems,
-    createItem
+    createItem,
+    updateItem
+
 };
