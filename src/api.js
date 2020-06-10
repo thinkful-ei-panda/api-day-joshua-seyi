5b8f4bee76f74ba33c5fbe2ac9c97540e8cd8e52
SeyiAriyo <ariyoseyiariyo@gmail.com>
2020-06-10 13:57:25 -0400

    Revert "Removing api module that isn't supposed to exist yet."
    This reverts commit cc6e135ca8c615adf8b8a68be14c2ec153d2a082.

---
+++ src/api.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[yourname]';
+
+const getItems = function () {
+  return Promise.resolve('A successful response!');
+};
+
+export default {
+  getItems
+};
