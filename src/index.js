898d9fca9c5b35fed93d9b2a7669eada29a4cf04
SeyiAriyo <ariyoseyiariyo@gmail.com>
2020-06-10 15:57:22 -0400

    part 5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import api from './api';
+import store from './store';
 
 import 'normalize.css';
 import './index.css';
@@ -7,7 +8,12 @@ import './index.css';
 import shoppingList from './shopping-list';
 
 const main = function () {
-
+  api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
