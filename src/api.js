b84ab998aae7224fe359cadb090a794d0b392bde
SeyiAriyo <ariyoseyiariyo@gmail.com>
2020-06-10 15:13:13 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,9 +3,6 @@ const BASE_URL = 'https://thinkful-list-api.herokuapp.com/joshuastruve';
 const getItems = function () {
   return fetch(`${BASE_URL}/items`);
 };
-function getItems() {
-    return fetch(`${BASE_URL}/items`);
-}
 
 export default {
   getItems
